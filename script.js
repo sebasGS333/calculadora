46ad0ee56c6457f4bda03b6c669826b38fe2b6cd
sebasGS333 <sebastiangallegosalazar@gmail.com>
2024-06-15 09:42:19 -0500

    cmbio potencia

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function dividir() {
 function potencia() {
   var num1 = parseFloat(document.getElementById('num1').value);
   var num2 = parseFloat(document.getElementById('num2').value);
-  var resultado = math.pow (num1 / num2);
+  var resultado = math.pow (num1, num2);
   document.getElementById('resultado').innerText = resultado;
 }
 
